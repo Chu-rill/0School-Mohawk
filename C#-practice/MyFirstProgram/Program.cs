472de781caf858fc0f63ffdb15bc28fab706f167
Chu-rill <churchillrimamskep@gmail.com>
2026-08-17 09:50:32 -0400

    getting started with data types

---
+++ C#-practice/MyFirstProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿int x; //declaration
+x =  123; //initilization
+
+int y = 13;
+
+int z = x + y;
+
+int age = 21;
+double height = 188.5;
+bool alive = true; 
+char symbol = '@';
+String name = "bob";
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(alive);
+Console.WriteLine(symbol);
